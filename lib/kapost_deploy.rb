b4638fe7a7598ad104632a5b4a37e46102345a2d
Brooke Kuhlmann <brooke.kuhlmann@kapost.com>
2016-05-23 21:03:50 -0600

    Updated pragma formatting.
    - Applied [Pragmater](https://github.com/bkuhlmann/pragmater)
      formatting by running the following:
      `pragmater -a . -c "frozen_string_literal: true" -w ".rb"`
    - NOTE: Support will be added in Gemsmith in the future so more
      automation will be available when building a gem to maintain
      consistency (Rubocop does help with some of this detection too).

--- lib/kapost_deploy.rb
+++ lib/kapost_deploy.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
+
 require "kapost_deploy/identity"
 require "kapost_deploy/task"
--- lib/kapost_deploy/heroku/app_promoter.rb
+++ lib/kapost_deploy/heroku/app_promoter.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require "platform-api"
 
 module KapostDeploy
